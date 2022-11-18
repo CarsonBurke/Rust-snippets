82e27e14059522ac079e27e805255b703d21003e
Carson <carsonburke22@gmail.com>
2022-11-18 10:15:12 -0800

    Update main.rs

--- snippets/grid generator/src/main.rs
+++ snippets/grid generator/src/main.rs
@@ -1,12 +1,27 @@
-#![allow(unused_imports)]
-#![allow(dead_code)]
-
-mod neural_network;
-
-use neural_network::{NeuralNetwork};
-
 fn main() {
-    println!("Hello, world!");
 
-    /* let neural_network_manager = NeuralNetworkManager::new(); */
+    let mut grid: Vec<Vec<usize>> = vec![];
+    
+    let x_max = 50;
+    let y_max = 50;
+    
+    let mut x = 0;
+    
+    while x < x_max {
+        
+        grid.push(vec![]);
+        
+        let mut y = 0;
+        
+        while y < y_max {
+            
+            grid[x].push(y);
+            
+            y += 1;
+        }
+        
+        println!("{:?}", grid[x]);
+        
+        x += 1;
+    }
 }
